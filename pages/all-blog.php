298f34e5a2f1c616492ca7c2517730f63e1c46f8
Your Name <rakibulhasancse21@gmail.com>
2022-10-31 09:00:32 +0600

    'substr-change'

--- pages/all-blog.php
+++ pages/all-blog.php
@@ -10,13 +10,8 @@
                     <div class="card-body">
                         <p><span class="fw-bold">Id :</span><?php echo $blog['id'];?></p>
                         <p><span class="fw-bold">Name :</span><?php echo $blog['name'];?></p>
-                        <p><span class="fw-bold">Roll :</span><?php echo $blog['roll'];?></p>
-                        <p><span class="fw-bold">Email :</span><?php echo $blog['email'];?></p>
-                        <p><span class="fw-bold">Section :</span><?php echo $blog['section'];?></p>
-                        <p><span class="fw-bold">Mobile :</span><?php echo $blog['Mobile'];?></p>
-                        <p><span class="fw-bold">Prresent Address :</span><?php echo $blog['present-address'];?></p>
-                        <p><span class="fw-bold">Permanent Address :</span><?php echo $blog['permanant-address'];?></p>
-                        <p><span class="fw-bold">About :</span><?php echo substr($blog['about'],0,50).'....' ;?></p>
+                        <p><span class="fw-bold">Roll :</span><?php echo substr($blog['roll'],0,4).'....';?></p>
+
                         <a href="action.php?page=details&&id=<?php echo $blog['id'] ?>" class="btn btn-outline-primary px-5">Read More</a>
                     </div>
                 </div>
